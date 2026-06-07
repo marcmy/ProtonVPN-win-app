1fd005498761f70b5b61a66d79b12887e1a3d755
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-07 12:33:29 -0400

    Repair assembly version helper

--- src/Common/ProtonVPN.Common.Core/Helpers/AssemblyVersion.cs
+++ src/Common/ProtonVPN.Common.Core/Helpers/AssemblyVersion.cs
@@ -11,4 +11,37 @@
  * ProtonVPN is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Reflection;
+
+namespace ProtonVPN.Common.Core.Helpers;
+
+public static class AssemblyVersion
+{
+    private static readonly Lazy<string> _version = new(CreateVersion);
+
+    private static string CreateVersion()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        AssemblyInformationalVersionAttribute? informationalVersion =
+            assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+
+        string? version = informationalVersion?.InformationalVersion;
+        if (!string.IsNullOrWhiteSpace(version))
+        {
+            return version;
+        }
+
+        return (assembly.GetName().Version ?? new()).ToString(3);
+    }
+
+    public static string Get()
+    {
+        return _version.Value;
+    }
+}
