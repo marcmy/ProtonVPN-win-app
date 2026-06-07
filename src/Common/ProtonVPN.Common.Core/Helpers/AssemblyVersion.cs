61c86950a839d20618689fc6d15fe99e2e72b6d1
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-07 12:32:32 -0400

    Show informational fork version

--- src/Common/ProtonVPN.Common.Core/Helpers/AssemblyVersion.cs
+++ src/Common/ProtonVPN.Common.Core/Helpers/AssemblyVersion.cs
@@ -11,27 +11,4 @@
  * ProtonVPN is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-using System.Reflection;
-
-namespace ProtonVPN.Common.Core.Helpers;
-
-public static class AssemblyVersion
-{
-    private static Lazy<string> _version = new Lazy<string>(CreateVersion);
-
-    private static string CreateVersion()
-    {
-        return (Assembly.GetExecutingAssembly().GetName().Version ?? new()).ToString(3);
-    }
-
-    public static string Get()
-    {
-        return _version.Value;
-    }
-}
+ * GNU General
