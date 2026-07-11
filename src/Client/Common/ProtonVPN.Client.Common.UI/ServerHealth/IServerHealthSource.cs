6cd7791c456b96eed279e464fb7b373d5832a3d3
marcmy <21000174+marcmy@users.noreply.github.com>
2026-07-11 12:44:44 -0400

    feat: add server health source contract

---
+++ src/Client/Common/ProtonVPN.Client.Common.UI/ServerHealth/IServerHealthSource.cs
@@ -0,0 +1,9 @@
+namespace ProtonVPN.Client.Common.UI.ServerHealth;
+
+public interface IServerHealthSource
+{
+    string HealthServerId { get; }
+    string? HealthProbeAddress { get; }
+    double HealthServerLoad { get; }
+    Task<ServerHealthProbeMeasurement> ProbeHealthAsync(CancellationToken cancellationToken);
+}
