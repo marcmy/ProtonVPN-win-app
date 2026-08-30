70978b88d075be2972f4772cc6e456e79c08f6f3
marcmy <21000174+marcmy@users.noreply.github.com>
2026-08-30 05:21:32 -0400

    Add structured client global exception handler

---
+++ src/Client/Common/ProtonVPN.Client.Common.UI/Exceptions/ClientGlobalExceptionHandler.cs
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025 Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#nullable enable annotations
+
+using Microsoft.UI.Xaml;
+using ProtonVPN.Logging.Contracts;
+using ProtonVPN.Logging.Contracts.Events.AppLogs;
+using ProtonVPN.Logging.Events;
+
+namespace ProtonVPN.Client.Common.UI.Exceptions;
+
+public class ClientGlobalExceptionHandler : GlobalExceptionHandlerBase
+{
+    public void Initialize(Application app)
+    {
+        base.Initialize();
+        app.UnhandledException += OnUiUnhandledException;
+    }
+
+    private void OnUiUnhandledException(object? sender, UnhandledExceptionEventArgs eventArgs)
+    {
+        TryLogException("UI unhandled exception", eventArgs.Exception, isFatal: false);
+
+        // Proton VPN 5.1.6 also sets eventArgs.Handled = true here. That crash-suppression
+        // policy is intentionally withheld until it is validated independently; the
+        // centralized logging architecture does not require swallowing every WinUI exception.
+    }
+
+    protected override void LogFatalException(ILogger logger, string handler, Exception? exception)
+    {
+        logger.Fatal<AppCrashLog>(handler, exception);
+    }
+
+    protected override void LogNonFatalException(ILogger logger, string handler, Exception? exception)
+    {
+        logger.Error<AppLog>(handler, exception);
+    }
+}
