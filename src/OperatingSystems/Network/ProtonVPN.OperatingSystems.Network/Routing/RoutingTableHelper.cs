374d17bcea8e23417b1d3a90031c02c4e288a85f
marcmy <21000174+marcmy@users.noreply.github.com>
2026-08-31 17:31:26 -0400

    Track server health route creation ownership explicitly

--- src/OperatingSystems/Network/ProtonVPN.OperatingSystems.Network/Routing/RoutingTableHelper.cs
+++ src/OperatingSystems/Network/ProtonVPN.OperatingSystems.Network/Routing/RoutingTableHelper.cs
@@ -42,6 +42,11 @@ public class RoutingTableHelper : IRoutingTableHelper
 
     public void CreateRoute(RouteConfiguration route)
     {
+        _ = TryCreateRoute(route);
+    }
+
+    public bool TryCreateRoute(RouteConfiguration route)
+    {
         InitializeIpForwardEntry(out MIB_IPFORWARD_ROW2 row);
 
         row.DestinationPrefix = GetDestinationPrefix(route);
@@ -52,7 +57,7 @@ public class RoutingTableHelper : IRoutingTableHelper
         row.PreferredLifetime = uint.MaxValue;
         row.Loopback = route.Gateway is null;
 
-        CreateIpForwardEntry2(ref row);
+        return CreateIpForwardEntry2(ref row).Succeeded;
     }
 
     public uint? GetLoopbackInterfaceIndex()
